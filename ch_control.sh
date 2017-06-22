ad4839c8815a7b2536aacf363a9b96c533e29b57
talcerito <tiago.alcerito@hotmail.com>
2017-06-22 16:52:12 -0300

    Delete ch_control.sh

--- ch_control.sh
+++
@@ -1,1 +0,0 @@
-sed -i  's/CTMPERMHOSTS   apscor00403p.intraservice.corp/CTMPERMHOSTS apscor00403p.intraservice.corp|apscor00404p.intraservice.corp|apscor00403b.intraservice.corp|apscor00404b.intraservice.corp/' /export/ctmag630/ctm/data/CONFIG.dat
